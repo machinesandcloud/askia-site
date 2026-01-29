c75520c10d2affad34778f409741d9f517f411ba
Steve Ngoumnai <stevengoumnai@Steves-MacBook-Pro.local>
2026-01-29 04:58:55 -0600

    Align SEO structure and add new service/role pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "admin": "admin" });
+  eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
+  eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
 
   eleventyConfig.addFilter("repeat", (value, times) => {
     return new Array(times).fill(value).join("");
