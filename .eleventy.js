67e3e62adef70665e2d49bd175ca4fd222f5088d
Steve Ngoumnai <stevengoumnai@Steves-MacBook-Pro.local>
2026-02-09 18:28:54 -0600

    Fix missing assets and legacy slug redirects

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "admin": "admin" });
   eleventyConfig.addPassthroughCopy({ "src/_redirects": "_redirects" });
   eleventyConfig.addPassthroughCopy({ "src/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "src/apple-touch-icon.png": "apple-touch-icon.png" });
+  eleventyConfig.addPassthroughCopy({ "src/apple-touch-icon-precomposed.png": "apple-touch-icon-precomposed.png" });
 
   eleventyConfig.addFilter("repeat", (value, times) => {
     return new Array(times).fill(value).join("");
